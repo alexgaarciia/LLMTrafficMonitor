b1a624dec4e70d642df74c9c709c7d2dba833d93
Alejandro <al.garcia636@gmail.com>
2024-09-22 18:45:48 +0200

    api key setup (tried it with lm studio but they only allow specific vendors)

--- main.R
+++ main.R
@@ -1,4 +1,7 @@
 # Install library directly from GitHub
 devtools::install_github("AlbertRapp/tidychatmodels")
+library(tidychatmodels)
 
-# 
+# API Key Setup
+usethis::edit_r_environ()
+my_api_key <- Sys.getenv('lm-studio')
