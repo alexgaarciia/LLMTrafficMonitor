ddb17ad3b06b339162d891065f4820687282baaf
Alejandro L. <al.garcia636@gmail.com>
2024-11-28 17:46:27 +0100

    formatting and printing style modified

--- langchain/Querying_Agent_v2_json.py
+++ langchain/Querying_Agent_v2_json.py
@@ -1,22 +1,19 @@
+import time
+import json
 import requests
 import urllib.parse
 
-import time
-
-import json
-
 # Read the JSON lines from the file
-with open('data/test.jsonl', 'r') as file:
+with open('../data/test.jsonl', 'r') as file:
     questions = []
     for line in file:
         entry = json.loads(line)
         questions.append(entry['question'])
 
-    
+
 def querying_agent():
-
+    counter = 1
     for question in questions[1:5]:
-
         query = str(question)
         # Create the URL with the encoded query
         url = f"http://127.0.0.1:3836/respond?query={urllib.parse.quote(query)}"
@@ -25,29 +22,29 @@ def querying_agent():
         request_size = len(url.encode('utf-8'))
         print(f"Request size (bytes): {request_size}")
 
-        # Send the GET request
+        # Send the GET request and check if the response was successful
         response = requests.get(url)
-        # Check if the response was successful
         if response.status_code != 200:
             print(f"Error: {response.status_code}: {response.reason}")
             continue  # Skip this iteration and ask for another query
 
         # Mistral
-        print("1:", response.content)
+        print("Model Name: Mistral")
+        print(f"Question number: {counter} \n", response.content)
         content = response.content
         response_size = len(content.decode('utf-8'))
         print(f"Response size (bytes): {response_size}")
-        time.sleep(5)        
-        
-        #OpenAI
-        # Parse the JSON response
-        # content = response.json()
-        #print("Response:", content.get("response"))
-        #response_size = len(response.text.encode('utf-8'))
-        #print(f"Response size (bytes): {response_size}")
-        
-    
-        
+        time.sleep(5)
+
+        # OpenAI
+        # print("Model Name: OpenAI")
+        # content = response.json()  # Parse the JSON response
+        # print("Response:", content.get("response"))
+        # response_size = len(response.text.encode('utf-8'))
+        # print(f"Response size (bytes): {response_size}")
+
+        counter += 1
+        print()
 
 
 # Run the querying agent
