300d6f41898ac01e8bef9d8deccb3685bde88d99
NataliaBlueCloud <140353434+NataliaBlueCloud@users.noreply.github.com>
2025-04-16 21:16:02 +0200

    Modified querying agent

---
+++ langchain/Querying_Agent_v2.py
@@ -0,0 +1,43 @@
+import time
+import json
+import requests
+import urllib.parse
+
+# Read the JSON lines from the file
+with open('../data/test.jsonl', 'r') as file:
+    questions = []
+    for line in file:
+        entry = json.loads(line)
+        questions.append(entry['question'])
+
+
+def querying_agent():
+    counter = 1
+    for question in questions[1:1000]:
+        query = str(question)
+        
+        # Create the URL with the encoded query
+        url = f"http://127.0.0.1:3836/respond?query={urllib.parse.quote(query)}"
+
+        # Print the size of the request
+        request_size = len(url.encode('utf-8'))
+        print(f"Request size (bytes): {request_size}")
+
+        # Send the GET request and check if the response was successful
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}: {response.reason}")
+            continue  # Skip this iteration and ask for another query
+
+        print(f"Question number: {counter} \n", response.content)
+        content = response.content
+        response_size = len(content.decode('utf-8'))
+        print(f"Response size (bytes): {response_size}")
+        time.sleep(10)
+
+        counter += 1
+        print()
+
+
+# Run the querying agent
+querying_agent()
